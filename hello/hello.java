5f1a228a59a9896dfc3f97d767cb9de540f04099
aa-ryan <aryansinha6363@gmail.com>
2021-07-02 11:22:46 +0530

    HelloClass added

---
+++ hello/hello.java
@@ -0,0 +1,7 @@
+/* If using public class name of the file should be same as name of class */
+public class hello {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello class");
+	}
+}
